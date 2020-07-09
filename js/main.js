bfc0b7ebbce134b367a2728f094c1f23b08297eb
lmh1989 <lmh2008@live.co.uk>
2020-07-09 09:49:48 +0100

    changed absolute links to relative links.

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 
             div.classList.add("col-12", "col-md-6", "col-lg-4", "mb-5", "text-center");
 
-            div.innerHTML = "<div class='indivRecipe' data-aos='fade-left'><a href='/Nutsapps/recipe.html?=" + data[i].indexVal + "'>" + "<div class='item-wrapper mb-0'>" + "<img class='main' src='" + data[i].image_src + "'/><span>View Recipe</span>" + "</div><div class='pt-4 pb-4 pl-2 pr-2 blue-bg'><p class='recipe-name mb-1'>" + data[i].title + "</p>" + "<p class='small mb-0'>By: " + data[i].author.name + "</p></div></a></div>";
+            div.innerHTML = "<div class='indivRecipe' data-aos='fade-left'><a href='recipe.html?=" + data[i].indexVal + "'>" + "<div class='item-wrapper mb-0'>" + "<img class='main' src='" + data[i].image_src + "'/><span>View Recipe</span>" + "</div><div class='pt-4 pb-4 pl-2 pr-2 blue-bg'><p class='recipe-name mb-1'>" + data[i].title + "</p>" + "<p class='small mb-0'>By: " + data[i].author.name + "</p></div></a></div>";
             mainContainer.appendChild(div);
         }
 
